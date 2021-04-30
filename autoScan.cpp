3dd884463885e3d40445be411a1b111db2ed2a4d
Luka-Hadzic <59064238+Luka-Hadzic@users.noreply.github.com>
2021-04-30 08:04:42 -0400

    Add files via upload

--- autoScan.cpp
+++ autoScan.cpp
@@ -6,9 +6,10 @@
 #include <iostream>
 
 typedef std::vector<std::string> stringVec;
+typedef std::vector<int> numVec;
 
-bool verbosity = true;
-bool boutput = true;
+bool verbosity = false;
+bool boutput = false;
 bool defaultParams = true;
 
 stringVec nmapScanSwitches
@@ -22,10 +23,22 @@ stringVec nmapScanSwitches
     "-sM"/* TCP Maimon port scan */,
 };
 
+numVec defaultScanNumVec
+{
+    1,
+    2,
+    3
+};
+
 stringVec nmapPortSwitches
 {
     "-p-"/* Port scan all ports */,
     "-F"/* Fast scan, first 100 */,
+};
+
+numVec defaultPortNumVec
+{
+    1
 };
 
 stringVec nmapServiceSwitches
@@ -35,7 +48,11 @@ stringVec nmapServiceSwitches
     "-A"/* Enables OS detection, version detection, script scanning, and traceroute */
 };
 
-
+numVec defaultServiceNumVec
+{
+    0,
+    1
+};
 
 stringVec digSwitches
 {
@@ -46,6 +63,13 @@ stringVec digSwitches
     "ns"/* find NS record */,
     "a"/* find A record */,
     "+trace"/* traces DNS path */
+};
+
+numVec defaultDigNumVec
+{
+    2,
+    3,
+    5
 };
 
 void startup()
@@ -143,7 +167,21 @@ void nmapScan()
 {
     if (defaultParams)
     {
-        /* code */
+        if (verbosity) // print out the thing
+        {
+            for (size_t i = 0; i < defaultScanNumVec.size(); i++)
+            {
+                std::cout << "nmap <target> " << nmapScanSwitches[defaultScanNumVec[i]] << std::endl;
+            }
+        }
+        else // proceed w/o printing
+        {
+            for (size_t i = 0; i < defaultScanNumVec.size(); i++)
+            {
+                //std::cout << "nmap <target> " << nmapScanSwitches[defaultScanNumVec[i]] << std::endl;
+            }
+        }
+         
     } else
     {
         /* code */
@@ -176,19 +214,19 @@ void scanSetter(int nmap, int dig, int nikto)
 {
     if (nmap == 1)
     {
-        std::cout << "Starting nMap Scan..." << std::endl;
+        std::cout << "\nStarting nMap Scan..." << std::endl;
         nmapScan();
     }
 
     if (dig == 1)
     {
-        std::cout << "Starting Dig Scan..." << std::endl;
+        std::cout << "\nStarting Dig Scan..." << std::endl;
         digScan();
     }
 
     if (nikto == 1)
     {
-        std::cout << "Starting Nikto Scan..." << std::endl;
+        std::cout << "\nStarting Nikto Scan..." << std::endl;
         niktoScan();
     }  
 }
@@ -204,21 +242,19 @@ void mainMenu(int caseVar)
         std::cout << "Beginning full scan with default parameters..." << std::endl;
         std::cout << "Results will be written to results.txt file..." << std::endl;
         std::cout << "results.txt wil be cleared on next startup..." << std::endl;
-        verbosity = false;
-        boutput = false;
+        scanSetter(1, 1, 1);
         break;
     
     case 2:
         std::cout << "Select which scans to run nMap[0(N)/1(Y)], Dig[0(N)/1(Y)], Nikto[0(N)/1(Y)]" << std::endl;
         std::cin >> x >> y >> z; //pass this to selector function
-        verbosity = false;
-        boutput = false;
         scanSetter(x, y, z);
         break;
     
     case 3:
         defaultParams = false;
         std::cout << "hello" << std::endl;
+        scanSetter(1, 1, 1);
         break;
     
     case 4:
